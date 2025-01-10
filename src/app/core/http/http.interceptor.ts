75c3235f1106fe808deb15656bcf534e2644b7ba
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-10 18:21:36 +0100

    Update: Enhance HTTP Interceptor with Error Handling
    - Updated `HttpInterceptorService` to intercept HTTP requests and handle `401 Unauthorized` errors by redirecting users to the login page.
    - Added global error handling to log other HTTP errors for debugging purposes.

--- src/app/core/http/http.interceptor.ts
+++ src/app/core/http/http.interceptor.ts
@@ -1,5 +1,34 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
-export const httpInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class HttpInterceptorService implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const clonedRequest = req.clone({ withCredentials: true });
+
+    return next.handle(clonedRequest).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.router.navigate(['/login']);
+        } else {
+          console.error('HTTP Error:', error);
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
