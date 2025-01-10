79d4cb630a9d77d758dce5fedd75d51ece5c35c5
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-10 18:18:07 +0100

    Update: Add HTTP Interceptor in appConfig
    - Updated `app.config.ts` to include `HttpInterceptorService` as an HTTP interceptor.
    - Added `HTTP_INTERCEPTORS` to the providers array in `appConfig`.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { routes } from './app.routes';
+import { HttpInterceptorService } from './core/http/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true,
+    },
+  ],
 };
