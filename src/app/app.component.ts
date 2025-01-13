6d42d9f75d9759e69ebcfaa8b94973d27068922a
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-13 07:58:40 +0100

    Refactor: Update AppComponent for Session Check
    - Imported `OnInit` lifecycle hook and `UserService` in `AppComponent`.
    - Updated `AppComponent` to implement `OnInit` for checking user session on initialization.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './core/services/user.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'UserSphere';
+export class AppComponent implements OnInit {
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.checkSession().subscribe();
+  }
 }
