b9596c17a177facd95c30120e19ae32f07e7a65c
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-13 08:21:51 +0100

    Update: Handle User Role and Navigation in AppComponent
    - Modified `AppComponent` to navigate based on user role after checking session.
    - If logged in, users are redirected to either `admin-dashboard` or `user-dashboard` based on role.
    - If not logged in, users are redirected to the `login` page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { UserService } from './core/services/user.service';
 
 @Component({
@@ -9,9 +9,23 @@ import { UserService } from './core/services/user.service';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
-    this.userService.checkSession().subscribe();
+    this.userService.checkSession().subscribe((isLoggedIn) => {
+      if (isLoggedIn) {
+        const role = this.userService.getUserRole();
+        console.log('User is logged in with role:', role);
+
+        if (role === 'admin') {
+          this.router.navigate(['/admin-dashboard']);
+        } else if (role === 'user') {
+          this.router.navigate(['/user-dashboard']);
+        }
+      } else {
+        console.log('User is not logged in');
+        this.router.navigate(['/login']);
+      }
+    });
   }
 }
