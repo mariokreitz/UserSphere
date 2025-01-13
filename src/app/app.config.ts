cabed95140f6f94a4bb847bde9078e3b554b2f7e
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-13 09:05:44 +0100

    feat: Add Angular Material to the project
    - Installed Angular Material and added it to the project.
    - You can now use Angular Material components for a more polished UI.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { HttpInterceptorService } from './core/http/http.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,6 @@ export const appConfig: ApplicationConfig = {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpInterceptorService,
       multi: true,
-    },
+    }, provideAnimationsAsync(),
   ],
 };
