3871b704bff4a028cbbcfd3cecfbb43ff1936615
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-14 07:58:25 +0100

    feat: Add register route
    - Updated application routes to include the `/register` path.
    - Associated the `RegisterComponent` with the new route.
    - Users can now navigate to `/register` to access the registration page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { AdminDashboardComponent } from './features/admin-dashboard/admin-dashbo
 import { UserDashboardComponent } from './features/user-dashboard/user-dashboard.component';
 import { AdminGuard } from './core/auth/admin-guard.guard';
 import { UserGuard } from './core/auth/user-guard.guard';
+import { RegisterComponent } from './features/register/register.component';
 
 export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   {
     path: 'admin-dashboard',
