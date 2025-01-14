5693683fb84f4163f3a4ae9cde71dc6a88470a69
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-14 11:40:05 +0100

    feat: Create User model
    - Added User interface to define the structure of user data.
    - The model includes fields like _id, email, firstName, lastName, username, profilePicture, role, isVerified, and timestamps (createdAt, updatedAt).

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,12 @@
+export interface User {
+  _id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  profilePicture: string;
+  role: string;
+  isVerified: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
