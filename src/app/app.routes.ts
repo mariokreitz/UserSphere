b0ef8e8e32757bfaae09785383d5983ce95fdf7f
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-14 10:14:04 +0100

    feat: Update routes to include NotFoundComponent
    - Updated routes to include the NotFoundComponent for handling 404 errors.
    - Renamed some route paths for better clarity and consistency.
    - Added a wildcard route (`**`) to catch any undefined paths and redirect to the NotFoundComponent.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,19 +5,28 @@ import { UserDashboardComponent } from './features/user-dashboard/user-dashboard
 import { AdminGuard } from './core/auth/admin-guard.guard';
 import { UserGuard } from './core/auth/user-guard.guard';
 import { RegisterComponent } from './features/register/register.component';
+import { UserManagementComponent } from './features/admin-dashboard/user-management/user-management.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   {
-    path: 'admin-dashboard',
+    path: 'admin',
     component: AdminDashboardComponent,
     canActivate: [AdminGuard],
+    children: [
+      {
+        path: 'management',
+        title: 'UserManagementComponent',
+        component: UserManagementComponent,
+      },
+    ],
   },
   {
-    path: 'user-dashboard',
+    path: 'user',
     component: UserDashboardComponent,
     canActivate: [UserGuard],
   },
-  { path: '**', redirectTo: 'login' },
+  { path: '**', component: NotFoundComponent },
 ];
