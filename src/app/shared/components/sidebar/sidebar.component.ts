fcc3cfa89defcc805be1b784c47e9cd46e74e202
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-14 10:17:14 +0100

    feat: Enhance Sidebar component for dynamic user/admin menus
    - Updated sidebar HTML to use Angular Material components for navigation.
    - Introduced dynamic menu rendering based on user role (Admin or User).
    - Added menu items for User Management, Dashboard, User List, and Statistics for admins.
    - Simplified display logic with `hidden` property and `@Input()` for role-based visibility.
    - Applied basic styling to improve sidebar layout and UI.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule, RouterModule, MatListModule, MatIcon],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  @Input() isAdmin: boolean = false;
 
+  constructor() {}
+
+  get menuItems() {
+    const items = [
+      {
+        label: 'User Management',
+        link: './management',
+        icon: 'group',
+        hidden: !this.isAdmin,
+      },
+      {
+        label: 'Dashboard',
+        link: this.isAdmin ? '/admin' : '/user',
+        icon: 'dashboard',
+      },
+
+      {
+        label: 'User List',
+        link: '/user-list',
+        icon: 'list',
+        hidden: !this.isAdmin,
+      },
+      {
+        label: 'Statistics',
+        link: '/statistics',
+        icon: 'bar_chart',
+        hidden: !this.isAdmin,
+      },
+      { label: 'User Profile', link: '/profile', icon: 'person' },
+      { label: 'Settings', link: '/settings', icon: 'settings' },
+    ].filter((item) => !item.hidden);
+
+    return items;
+  }
 }
