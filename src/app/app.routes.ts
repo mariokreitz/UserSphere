2b686de29664d6ac22e0834f3bc98ea0b37f4caf
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-15 23:47:58 +0100

    feat: Add child route for Audit Logs in admin dashboard
    - Added new child route `/admin/audit` for viewing audit logs.
    - Integrated `AuditLogsComponent` to display logs and filter options.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,10 @@ import { UserDashboardComponent } from './features/user-dashboard/user-dashboard
 import { AdminGuard } from './core/auth/admin-guard.guard';
 import { UserGuard } from './core/auth/user-guard.guard';
 import { RegisterComponent } from './features/register/register.component';
-import { UserManagementComponent } from './features/admin-dashboard/user-management/user-management.component';
+import { UserManagementComponent } from './features/admin-dashboard/components/user-management/user-management.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
 import { ProfileOverviewComponent } from './features/profile-overview/profile-overview.component';
+import { AuditLogsComponent } from './features/admin-dashboard/components/audit-logs/audit-logs.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
@@ -26,6 +27,11 @@ export const routes: Routes = [
         path: 'profile',
         title: 'ProfileOverviewComponent',
         component: ProfileOverviewComponent,
+      },
+      {
+        path: 'audit',
+        title: 'AuditLogsComponent',
+        component: AuditLogsComponent,
       },
     ],
   },
