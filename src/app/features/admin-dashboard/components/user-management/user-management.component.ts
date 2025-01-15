980acc6efe2a20ce8362ff777be0ca71ccf2bf8d
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-15 23:52:34 +0100

    feat: Update UserManagementComponent to render all users from the database
    - The `UserManagementComponent` now fetches and displays a list of all current users from the database.
    - Integrated user data retrieval from the backend to dynamically populate the user list.

---
+++ src/app/features/admin-dashboard/components/user-management/user-management.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-user-management',
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatSelectModule,
+  ],
+  templateUrl: './user-management.component.html',
+  styleUrl: './user-management.component.scss',
+})
+export class UserManagementComponent implements OnInit {
+  users: any[] = [];
+  filters = { search: '', role: '' };
+  displayedColumns: string[] = ['username', 'email', 'role', 'actions'];
+
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.fetchUsers();
+  }
+
+  fetchUsers(): void {
+    this.adminService.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+      },
+      error: (err) => {
+        console.error('Error fetching users', err);
+      },
+    });
+  }
+
+  addUser(): void {}
+
+  editUser(): void {}
+
+  deleteUser(): void {}
+}
