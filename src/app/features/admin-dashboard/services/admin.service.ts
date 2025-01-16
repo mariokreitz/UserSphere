d6be15753b4b9155c02ddbe3b532d33feaa809a7
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-16 11:04:38 +0100

    chore: Remove unused import of Log model
    - Removed the unused import of `Log` from `../../../core/models/log.model` to clean up the code.

--- src/app/features/admin-dashboard/services/admin.service.ts
+++ src/app/features/admin-dashboard/services/admin.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
 import { User } from '../../../core/models/user.model';
-import { Log } from '../../../core/models/log.model';
 
 @Injectable({
   providedIn: 'root',
