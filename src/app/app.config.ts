54527b9ec1f901708623d58c04335922c35b5112
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-17 21:16:15 +0100

    chore: update app.config.ts for CSRF token handling and session config
    - Updated `app.config.ts` to configure CSRF token handling across the app.
    - Ensured session management and authentication flow are consistent with recent changes.
    - Adapted configurations to support secure cookie handling and user authentication.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,35 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpXsrfTokenExtractor,
+  provideHttpClient,
+  withXsrfConfiguration,
+} from '@angular/common/http';
 import { routes } from './app.routes';
 import { HttpInterceptorService } from './core/http/http.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { CustomXsrfTokenExtractor } from './core/http/custom-xsrf-token-extractor.interceptor'; // Your custom extractor
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(
+      withXsrfConfiguration({
+        cookieName: 'csrfToken',
+        headerName: 'X-Csrf-Token',
+      })
+    ),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpInterceptorService,
       multi: true,
+    },
+    {
+      provide: HttpXsrfTokenExtractor,
+      useClass: CustomXsrfTokenExtractor,
     },
     provideAnimationsAsync(),
     provideCharts(withDefaultRegisterables()),
