8cc0b073189f18555b4f804618217498e7bb8409
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-21 13:07:05 +0100

    refactor: update session and current user handling in app.component
    - Modified app.component to streamline session and current user verification logic.
    - Improved initialization flow to ensure consistent user state management.
    - Updated related service calls to align with the new implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from './core/services/auth.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,32 +13,31 @@ export class AppComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-    this.authService.checkSession().subscribe({
-      next: (loggedIn) => {
-        if (loggedIn) {
-          this.authService.getCurrentUser().subscribe({
-            next: (user) => {
-              if (user) {
-                const userRole = this.authService.getUserRole();
-                const route = userRole ? `/${userRole}` : '';
-                this.router.navigate([route]);
-              } else {
-                this.router.navigate(['/login']);
-              }
-            },
-            error: (err) => {
-              console.error('Error loading current user:', err);
-              this.router.navigate(['/login']);
-            },
-          });
-        } else {
+    this.authService
+      .checkSession()
+      .pipe(
+        switchMap((loggedIn) => {
+          if (loggedIn) {
+            return this.authService.getCurrentUser();
+          } else {
+            return [];
+          }
+        })
+      )
+      .subscribe({
+        next: (user) => {
+          if (user) {
+            const userRole = this.authService.getUserRole();
+            const route = userRole ? `/${userRole}` : '';
+            this.router.navigate([route]);
+          } else {
+            this.router.navigate(['/login']);
+          }
+        },
+        error: (err) => {
+          console.error('Failed to check session', err);
           this.router.navigate(['/login']);
-        }
-      },
-      error: (err) => {
-        console.error('Failed to check session', err);
-        this.router.navigate(['/login']);
-      },
-    });
+        },
+      });
   }
 }
