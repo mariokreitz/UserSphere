121320292df7e50670fe5f0f30a2a992a22ffab4
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-29 22:21:52 +0200

    feat: configure routes.ts with lazy-loaded components and add auth guard to protect dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (c) => c.RegisterComponent
+      ),
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+  },
+];
