76720efd33c94252d8a9ac05e43caa5bf4fb220e
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-29 19:49:25 +0200

    project reset

--- src/app/core/models/log.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Log {
-  _id: string;
-  user: string;
-  action: string;
-  details: string;
-  ip: string;
-  timestamp: string;
-}
--- src/app/features/user-settings/user-settings.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-user-settings',
-  imports: [],
-  templateUrl: './user-settings.component.html',
-  styleUrl: './user-settings.component.scss',
-})
-export class UserSettingsComponent {}
