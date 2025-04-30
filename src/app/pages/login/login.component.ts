64b64dbcf758975509c475a288eb59f2c69957ec
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-30 19:23:13 +0200

    feat: implement basic reactive form layout for register and login components

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  fb = inject(FormBuilder);
+
+  form = this.fb.nonNullable.group({
+    email: [''],
+    password: [''],
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validator } from '@angular/forms';
+import { environment } from '../../../environments/environment.development';
+import { UserInterface } from '../../types/user.interface';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
 
+  form = this.fb.nonNullable.group({
+    username: [''],
+    email: [''],
+    password: [''],
+  });
+
+  onSubmit() {
+    this.http
+      .post<UserInterface>(
+        `${environment.apiUrl}/auth/register`,
+        this.form.getRawValue(),
+        { withCredentials: true }
+      )
+      .subscribe((res) => console.log(res));
+  }
 }
