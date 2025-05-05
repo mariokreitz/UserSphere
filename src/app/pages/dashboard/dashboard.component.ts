c6a0d1563313d4ca798448341eea18b8155555d8
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-06 00:23:33 +0200

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  protected authService = inject(AuthService);
 
+  logout() {
+    this.authService.logout();
+  }
 }
