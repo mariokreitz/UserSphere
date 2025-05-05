91ede9b178fe5925ffe6b62e31523c2ca4b44d86
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-06 00:24:18 +0200

    reset

--- src/app/services/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, signal } from '@angular/core';
-import { UserInterface } from '../types/user.interface';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class UserService {
-  currentUser = signal<UserInterface | null | undefined>(undefined);
-}
