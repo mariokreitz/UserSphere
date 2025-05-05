c1afe20c569a11a29f21cc3a3c9621b36fae9c22
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-06 00:46:40 +0200

    chore: reset project and initialize with Angular, AngularFire (Firestore & Auth), NgRx SignalStore, and Angular Material

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'items');
+    this.items$ = collectionData(aCollection);
+  }
+}
