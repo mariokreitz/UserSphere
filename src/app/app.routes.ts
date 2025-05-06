9f691f51b320b39bf212cdcd528ed9b438058dfb
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-07 01:37:33 +0200

    feat: add routes for documentation, about, features, and signin pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,25 @@ export const routes: Routes = [
     component: HomeComponent,
   },
   {
+    path: 'documentation',
+    loadComponent: () =>
+      import('./routes/documentation/documentation.component').then(
+        (m) => m.DocumentationComponent
+      ),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./routes/about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'features',
+    loadComponent: () =>
+      import('./routes/features/features.component').then(
+        (m) => m.FeaturesComponent
+      ),
+  },
+  {
     path: 'signin',
     loadComponent: () =>
       import('./routes/signin/signin.component').then((m) => m.SigninComponent),
