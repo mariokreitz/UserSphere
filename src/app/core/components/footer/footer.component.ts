8585fc2554c959a7080bb3086ef3a299977c0287
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-09 00:36:47 +0200

    feat: add basic footer with imprint and privacy policy links

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
