c35eb0f208aa7a755c01e93536f1d5ffe34f4e57
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-08 10:23:59 +0200

    Create theme-switcher.component.ts

---
+++ src/app/core/components/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-switcher',
+  imports: [],
+  templateUrl: './theme-switcher.component.html',
+  styleUrl: './theme-switcher.component.scss'
+})
+export class ThemeSwitcherComponent {
+
+}
