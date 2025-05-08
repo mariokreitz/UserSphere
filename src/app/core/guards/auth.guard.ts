8b3f67c11a4b242c194488340f2d9fc3fac9bf66
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-09 00:38:01 +0200

    feat: implement AuthGuard with public route checks and redirect logic

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,34 @@
-import { CanActivateFn } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import {
+  CanActivate,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  protected router = inject(Router);
+  protected authService = inject(AuthService);
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    const publicRoutes = ['/', '/impressum', '/datenschutz'];
+
+    if (publicRoutes.some((route) => state.url.startsWith(route))) {
+      return true;
+    }
+
+    if (this.authService.isAuthenticated()) {
+      return true;
+    }
+
+    this.router.navigate(['/']);
+    return false;
+  }
+}
