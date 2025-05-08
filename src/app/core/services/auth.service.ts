b02d1091cb16ac068ec6ce9a1736473ec16791cd
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-09 00:38:12 +0200

    feat(auth): add signal-based isAuthenticated state to AuthService

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  public isAuthenticated = signal<boolean>(true);
 
-  constructor() { }
+  constructor() {}
 }
