3b3ba5c3ca88bf5094925359807bd262759caa8a
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-09 00:37:47 +0200

    feat: add ThemeSwitcher component with toggle using signal and effect

--- src/app/core/components/theme-switcher/theme-switcher.component.ts
+++ src/app/core/components/theme-switcher/theme-switcher.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-theme-switcher',
-  imports: [],
+  imports: [MatIcon, MatButtonModule],
   templateUrl: './theme-switcher.component.html',
-  styleUrl: './theme-switcher.component.scss'
+  styleUrl: './theme-switcher.component.scss',
 })
 export class ThemeSwitcherComponent {
+  darkMode = signal(false);
 
+  setDarkMode = effect(() => {
+    document.documentElement.classList.toggle('dark', this.darkMode());
+  });
 }
