ba78e29092f68c2cfc75fb9a0405f4e361e93f17
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-09 00:36:24 +0200

    feat: add NotFound component and configure fallback route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
+import { NotFoundComponent } from './routes/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -7,27 +8,17 @@ export const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'documentation',
-    loadComponent: () =>
-      import('./routes/documentation/documentation.component').then(
-        (m) => m.DocumentationComponent
-      ),
-  },
-  {
     path: 'about',
     loadComponent: () =>
       import('./routes/about/about.component').then((m) => m.AboutComponent),
   },
   {
-    path: 'features',
-    loadComponent: () =>
-      import('./routes/features/features.component').then(
-        (m) => m.FeaturesComponent
-      ),
-  },
-  {
     path: 'signin',
     loadComponent: () =>
       import('./routes/signin/signin.component').then((m) => m.SigninComponent),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
