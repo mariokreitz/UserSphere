8486ec690f4fa3ba0c8fe4124a3866f7781eeaf7
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-09 01:06:04 +0200

    update(home): replace placeholder text with hero section in home.component and add basic styling

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../core/components/header/header.component';
 import { FooterComponent } from '../../core/components/footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [RouterModule, MatButtonModule, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
