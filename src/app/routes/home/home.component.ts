9908a339dd8bef7d9e4312fb2f8075b729891276
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-12 23:26:38 +0200

    Update home.component.ts

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,11 +1,21 @@
 import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 import {RouterLink} from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   imports: [
-    RouterLink
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    RouterLink,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
