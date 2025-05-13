d4700b4cc9553a5d12977d0aafdc12958fa5a2fb
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-13 18:03:14 +0200

    Create snackbar.service.ts

---
+++ src/app/core/services/snackbar.service.ts
@@ -0,0 +1,36 @@
+import {inject, Injectable} from '@angular/core';
+import {MatSnackBar, MatSnackBarConfig} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SnackbarService {
+  private snackBar = inject(MatSnackBar);
+
+
+  show(
+    message: string,
+    action: string = '',
+    config: Partial<MatSnackBarConfig> = {}
+  ) {
+    const defaultConfig: MatSnackBarConfig = {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['app-snackbar'],
+    };
+
+    const mergedConfig = {...defaultConfig, ...config};
+    this.snackBar.open(message, action, mergedConfig);
+  }
+
+
+  success(message: string, action = 'OK') {
+    this.show(message, action, {panelClass: ['snackbar-success']});
+  }
+
+
+  error(message: string, action = 'RETRY') {
+    this.show(message, action, {panelClass: ['snackbar-error'], duration: 5000});
+  }
+}
