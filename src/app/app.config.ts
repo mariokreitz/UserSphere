42d519287284c741ba41bb03f52d6e6c54077221
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-13 18:03:10 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
-
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {routes} from './app.routes';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
@@ -9,6 +9,7 @@ import {environment} from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment)),
