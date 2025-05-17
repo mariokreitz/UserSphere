d623c0bb1df5072713f0db3332e8c80d1a1c8541
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-18 00:51:38 +0200

    Update auth.service.ts

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,18 +1,22 @@
-import {inject, Injectable} from '@angular/core';
-import {Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from '@angular/fire/auth';
-import {from} from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { from } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthService {
-  private auth = inject(Auth)
+    private auth = inject(Auth);
 
-  signIn(email: string, password: string) {
-    return from(signInWithEmailAndPassword(this.auth, email, password))
-  }
+    signIn(email: string, password: string) {
+        return from(signInWithEmailAndPassword(this.auth, email, password));
+    }
 
-  signUp(email: string, password: string) {
-    return from(createUserWithEmailAndPassword(this.auth, email, password))
-  }
+    signUp(email: string, password: string) {
+        return from(createUserWithEmailAndPassword(this.auth, email, password));
+    }
+
+    logout() {
+        return from(this.auth.signOut());
+    }
 }
