e5f9b7616e1b6a21b03594ca357492a021748145
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-17 22:24:55 +0200

    Update user.service.ts

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,14 +1,33 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { Auth, user } from '@angular/fire/auth';
+import { Auth, user, User as FirebaseUser } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { UserInterface } from '../../../models/types/UserInterface';
 
 @Injectable({
     providedIn: 'root',
 })
 export class UserService {
-    public currentUser = signal<UserInterface | null | undefined>(undefined);
+    public currentUser = signal<UserInterface | null>(null);
+    public auth = inject(Auth);
+    public readonly firebaseUser$ = user(this.auth);
+    public readonly isAuthenticated$: Observable<boolean> =
+      this.firebaseUser$.pipe(map(fbUser => !!fbUser));
 
-    private auth = inject(Auth);
-    
-    public user$ = user(this.auth);
+    constructor() {
+        this.firebaseUser$.subscribe((fbUser: FirebaseUser | null) => {
+            if (fbUser) {
+                this.currentUser.set({
+                    email: fbUser.email ?? '',
+                    username: fbUser.displayName ?? '',
+                });
+            } else {
+                this.currentUser.set(null);
+            }
+        });
+    }
+
+    public currentUserSync(): UserInterface | null {
+        return this.currentUser();
+    }
 }
