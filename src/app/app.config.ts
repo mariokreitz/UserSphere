33b9641d2a15141bb28599d34227494e676ace8e
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-17 22:24:51 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,21 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {routes} from './app.routes';
-import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
-import {getAuth, provideAuth} from '@angular/fire/auth';
-import {getFirestore, provideFirestore} from '@angular/fire/firestore';
-import {environment} from '../environments/environment.development';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
+import { environment } from '../environments/environment.development';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideAnimationsAsync(),
-    provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(environment)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
-  ]
+    providers: [
+        provideAnimationsAsync(),
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes, withEnabledBlockingInitialNavigation()),
+        provideFirebaseApp(() => initializeApp(environment)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+    ],
 };
 
 
