813cdb468490f8b7ee852cc1a5092aa00424e1af
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-18 01:02:50 +0200

    Update profile.component.ts

--- src/app/routes/profile/profile.component.ts
+++ src/app/routes/profile/profile.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-profile',
-  imports: [],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+    selector: 'app-profile',
+    imports: [],
+    templateUrl: './profile.component.html',
+    styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+    private route = inject(ActivatedRoute);
 
+    protected id: string = this.route.snapshot.params['id'];
 }
