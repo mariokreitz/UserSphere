6f3e82c126c1599a1283c6c25d1be15aa32303ba
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-17 21:24:41 +0200

    Update login.component.ts

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,89 +1,104 @@
-import {Component, inject, signal} from '@angular/core';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {RegisterForm} from '../../../models/types/registerForm';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {AuthService} from '../../core/services/auth.service';
-import {Router} from '@angular/router';
-import {SnackbarService} from '../../core/services/snackbar.service';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Router } from '@angular/router';
+import { RegisterForm } from '../../../models/types/registerForm';
+import { AuthService } from '../../core/services/auth.service';
+import { SnackbarService } from '../../core/services/snackbar.service';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatProgressSpinnerModule
-  ]
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+    ],
 })
 export class LoginComponent {
-  protected isPasswordVisible = signal(false);
-  protected isLoading = signal(false);
-  protected signupForm: FormGroup<RegisterForm>;
+    protected isPasswordVisible = signal(false);
+    protected isLoading = signal(false);
+    protected signupForm: FormGroup<RegisterForm>;
 
-  private auth = inject(AuthService);
-  private router = inject(Router);
-  private snackbarService = inject(SnackbarService);
+    private authService = inject(AuthService);
+    private userService = inject(UserService);
+    private router = inject(Router);
+    private snackbarService = inject(SnackbarService);
 
-  constructor() {
-    this.signupForm = new FormGroup({
-      email: new FormControl<string | null>(null, [Validators.required, Validators.email]),
-      password: new FormControl<string | null>(null, [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/), Validators.minLength(8)]),
-    });
-
-  }
-
-  get email() {
-    return this.signupForm.get('email');
-  }
-
-  get password() {
-    return this.signupForm.get('password');
-  }
-
-  togglePasswordVisibility() {
-    this.isPasswordVisible.update(value => !value);
-  }
-
-  onSubmit(): void {
-    if (this.signupForm.invalid) {
-      this.signupForm.markAllAsTouched();
-      return;
+    get email() {
+        return this.signupForm.get('email');
     }
 
-    this.isLoading.set(true);
+    get password() {
+        return this.signupForm.get('password');
+    }
 
-    const {email, password} = this.signupForm.value;
+    constructor() {
+        this.signupForm = new FormGroup({
+            email: new FormControl<string | null>(null, [
+                Validators.required,
+                Validators.email,
+            ]),
+            password: new FormControl<string | null>(null, [
+                Validators.required,
+                Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
+                Validators.minLength(8),
+            ]),
+        });
 
-    this.auth.signIn(email!, password!).subscribe({
-      next: (res) => {
-        this.isLoading.set(false);
-        this.snackbarService.success('Erfolgreich angemeldet');
-        this.router.navigateByUrl('/')
-      },
-      error: (err) => {
-        this.isLoading.set(false);
-        this.snackbarService.error("Anmeldung fehlgeschlagen");
-      }
-    })
+    }
 
+    togglePasswordVisibility() {
+        this.isPasswordVisible.update(value => !value);
+    }
 
-  }
+    onSubmit(): void {
+        if (this.signupForm.invalid) {
+            this.signupForm.markAllAsTouched();
+            return;
+        }
 
-  signInWithGoogle(): void {
-    console.log('Google Sign-In aufgerufen');
-  }
+        this.isLoading.set(true);
 
-  signInWithGithub(): void {
-    console.log('GitHub Sign-In aufgerufen');
-  }
+        const { email, password } = this.signupForm.value;
+
+        this.authService.signIn(email!, password!).subscribe({
+            next: (res) => {
+                this.isLoading.set(false);
+                this.snackbarService.success('Erfolgreich angemeldet');
+                this.router.navigateByUrl('/');
+            },
+            error: (err) => {
+                this.isLoading.set(false);
+                this.setFormError('password', 'invalidPassword', 'Benutzername oder Passwort sind falsch');
+            },
+        });
+
+    }
+
+    signInWithGoogle(): void {
+        console.log('Google Sign-In aufgerufen');
+    }
+
+    signInWithGithub(): void {
+        console.log('GitHub Sign-In aufgerufen');
+    }
+
+    setFormError(formControlName: string, errorType: string, errorMessage: string) {
+        const formControl = this.signupForm.get(formControlName);
+        if (formControl) {
+            formControl.setErrors({ [errorType]: errorMessage });
+        }
+    }
 }
