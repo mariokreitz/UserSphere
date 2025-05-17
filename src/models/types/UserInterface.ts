2c8046bb4f967f8c631c7512945e34f20688e40f
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-17 23:04:42 +0200

    Update UserInterface.ts

--- src/models/types/UserInterface.ts
+++ src/models/types/UserInterface.ts
@@ -1,4 +1,10 @@
+import { UserMetadata } from '@angular/fire/auth';
+
 export interface UserInterface {
+    uid: string;
     email: string;
     username: string;
+    emailVerified: boolean;
+    metadata: UserMetadata;
 }
+
