e8d8583b0a6167edd6ee3a02787024b6ba8db1db
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-17 21:24:49 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,32 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {HeaderComponent} from './core/components/header/header.component';
-import {FooterComponent} from './core/components/footer/footer.component';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { UserService } from './core/services/user.service';
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+        HeaderComponent,
+        FooterComponent,
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'usersphere';
+export class AppComponent implements OnInit {
+    private userService = inject(UserService);
+
+    ngOnInit(): void {
+        this.userService.user$.subscribe((user) => {
+            if (user) {
+                this.userService.currentUser.set({
+                    email: user.email!,
+                    username: user.displayName!,
+                });
+            } else {
+                this.userService.currentUser.set(null);
+            }
+        });
+    }
 }
